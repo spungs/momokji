b0ddd662226b406b18af9b98a27189d17cb9919b
ParkMs <bark4031402@gmail.com>
2024-03-06 15:51:33 +0900

    tets

--- src/main/java/comj/esc/momokji/controller/MainController.java
+++ src/main/java/comj/esc/momokji/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
+    // test
     @GetMapping("/")
     public String main() {
         return "main";
