a83dc00dcca938ffad06f3b15b271c2390053e84
spungs <7564s@naver.com>
2024-03-06 15:46:31 +0900

    init

---
+++ src/main/java/comj/esc/momokji/MomokjiApplication.java
@@ -0,0 +1,13 @@
+package comj.esc.momokji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomokjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomokjiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/comj/esc/momokji/controller/MainController.java
@@ -0,0 +1,13 @@
+package comj.esc.momokji.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
