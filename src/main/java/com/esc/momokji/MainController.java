315343e6b347fc13ba328a0ad37bbd9e1b4e4b74
spungs <7564s@naver.com>
2024-03-14 11:14:38 +0900

    카카오맵 api 소스 추가 박대표가 stash에 올려둠..

--- src/main/java/com/esc/momokji/MainController.java
+++ src/main/java/com/esc/momokji/MainController.java
@@ -1,12 +1,16 @@
 package com.esc.momokji;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Log4j2
 @Controller
 public class MainController {
+    @Value("appkey")
+    private String appkey;
 
 //    @GetMapping("/")
 //    public String main() {
@@ -17,4 +21,13 @@ public class MainController {
     public String chooseMenu() {
         return "chooseMenu";
     }
+<<<<<<< Updated upstream
+=======
+
+    @GetMapping("kakaoMap")
+    public String naverMap(Model model) {
+        model.addAttribute("appkey", appkey);
+        return "map/kakaoMap";
+    }
+>>>>>>> Stashed changes
 }
