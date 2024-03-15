3b16fde20a880519c9c33031b48cfaa43c820503
ParkMs <bark4031402@gmail.com>
2024-03-15 15:02:31 +0900

    * 변경내역 조회용 커밋

--- src/main/resources/static/js/headerLayout/headerLayout.js
+++ src/main/resources/static/js/headerLayout/headerLayout.js
@@ -2,11 +2,11 @@ $(document).ready(() => {
     let bannerEvent = $(".bannerEvent");
 
     bannerEvent.mouseover(() => {
-        let i = document.getElementById("whatImg");
-        let c = document.getElementById("categoryBtn");
+        let i = $("#whatImg");
+        let c = $("#categoryBtn");
 
-        // c.style.top = String(i.getBoundingClientRect().bottom - 18) + "px";
-        c.style.top = String(c.style.top - 18) + "px";
+        c.style.top = String(i.getBoundingClientRect().bottom - 18) + "px";
+        // c.style.top = String(c.style.top - 18) + "px";
         c.style.left = (i.getBoundingClientRect().left + 1) + "px";
 
         $("#categoryList").css("display", "block");
@@ -16,9 +16,27 @@ $(document).ready(() => {
         $("#categoryList").css("display", "none");
     });
 
-    $("#loginBtn").on("click", () => {
-        location.href = "/login";
+/*
+    let bannerEvent2 = $("#bannerEvent2");
+
+    bannerEvent2.mouseover(() => {
+        let l = $("#loginBtn");
+        let b = $("#loginBanner");
+
+        b.style.top = String(l.getBoundingClientRect().bottom + 18) + "px";
+        b.style.left = String(l.getBoundingClientRect().left - 18) + "px";
+
+        $("#loginPlatformList").style("display", "block");
     });
+
+    bannerEvent2.mouseout(() => {
+        $("#loginPlatformList").css("display", "none");
+    });
+
+    $("#loginBtn").mouseup(() => {
+
+    });
+*/
 
     $("#headerLogoText").on("click", () => {
         location.href = "/";
